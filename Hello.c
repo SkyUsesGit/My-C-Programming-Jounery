638ee8a88fd9c393bdc4c531fa1f5e9bc1eda7c3
Akash Maurya <103624106+SkyUsesGit@users.noreply.github.com>
2026-08-20 10:27:11 +0530

    Basic Hello World Print Statement In C
    This file contains basic Hello world program and also information about header files, pre processor directive , functions and datatype like void

---
+++ Hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> /* #include is the pre-proccessor directive that tells the compiler to exectue this line before the code 
+so that the content of header file that is our standard input output or stddio.h can be loaded and it's functions
+ can be called anywhere in the code*/
+void main(){/*void is the data type that we are using for the main function as we don't know / have specified a return type
+            main() a function and everything that we want our code to do is written in this funtion  */
+            printf("Hello World");/*The printf funtion helps us to display our desired output on the users screen , syntax can be seen as well*/
+}
