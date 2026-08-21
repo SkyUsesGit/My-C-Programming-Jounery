f454cd26437e67d398fc61a4668039461d2bff18
Akash Maurya <103624106+SkyUsesGit@users.noreply.github.com>
2026-08-21 19:39:02 +0530

    Basic addition of two numbers
    This file contains the basic addition code of two numbers.
    Here the two numbers were already defined ,
    In future codes we will be taking the numbers as input from user.
    :)

---
+++ Sum of two numbers.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    int num1 , num2 , sum;//Declaring the datatype and the variables for the two numbers and their sum
+    num1 = 10; //Giving the value of first number right now . In later codes we will take the number as input from the user
+    num2 = 5;
+    sum = num1 + num2;//Calculating the sum of the two numbers 
+    printf("The sum of %d and %d is : %d\n" , num1 , num2 , sum);//printing the output
+}
